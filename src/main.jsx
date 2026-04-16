d2469a2a9fe3fb8ed160358e50429b2553f437ee
Ronitraj Sinha <sinharonitraj@gmail.com>
2026-04-16 17:24:55 +0530

    fix: remove duplicate BrowserRouter and HelmetProvider wrapping in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 import "./styles/glass.css";
@@ -9,10 +7,6 @@ import "./styles/animations.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
